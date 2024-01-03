def9896dbffa09d9b4b9bcfa3e30b842ef9fa356
Matias Flores <matias.flores@logichainge.com>
2024-01-03 23:21:04 +0100

    Pizarra blog upload

---
+++ Pizarra_Blog/README.md
@@ -0,0 +1,38 @@
+<div align="center">
+<h1 align="center">
+  <br>
+<img src="https://git.fhict.nl/I437632/pizarra-blog/-/raw/master/images/Markr_0.png" alt="Pizarra Blog" align="center" width="800"></img>
+  <br>
+  Pizarra Inc. 
+  <br>
+</h1>
+
+<h4 align="center">SCRIBBLE YOUR DREAMS</h4>
+</div>
+
+
+## What is Pizarra
+<table>
+<tr>
+<td>
+  A blog dedicated to Pizarra Inc. where you can find all our updates on work, team and our links. At Pizarra our goal is to transform your initial ideas into the perfect formula. We understand that sometimes ideas don't leave the whiteboard, but with the perfect swift, they can transform into the perfect product.
+</td>
+</tr>
+</table>
+
+## Site
+
+http://i437632.hera.fhict.nl/index.html
+
+
+## Team
+- Matias Flores - 437632@student.fontys.nl
+- Anastasia Putuntica - a.putuntica@student.fontys.nl
+- Daniel Vaswani - d.vaswani@student.fontys.nl
+- Luciano de Armas - l.dearmas@student.fontys.nl
+- Sarvin Satchithanantham - s.satchithanantham@student.fontys.nl
+
+## License
+
+Pizarra Inc. © 
+
---
+++ Pizarra_Blog/pz.js
@@ -0,0 +1,112 @@
+//////////NAVBAR FADE//////////////////
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navigation").style.top = "0";
+//   } else {
+//     document.getElementById("navigation").style.top = "-50px";
+//   }
+// }
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+////////SLIDER////////
+const items = document.querySelectorAll('.item'),
+  controls = document.querySelectorAll('.control'),
+  headerItems = document.querySelectorAll('.item-header', '.item-header1'),
+  descriptionItems = document.querySelectorAll('.item-description'),
+  activeDelay = .76,
+  interval = 5000;
+
+let current = 0;
+
+const slider = {
+  init: () => {
+    controls.forEach(control => control.addEventListener('click', (e) => { slider.clickedControl(e) }));
+    controls[current].classList.add('active');
+    items[current].classList.add('active');
+  },
+  nextSlide: () => { // Increment current slide and add active class
+    slider.reset();
+    if (current === items.length - 1) current = -1; // Check if current slide is last in array
+    current++;
+    controls[current].classList.add('active');
+    items[current].classList.add('active');
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+  },
+  clickedControl: (e) => { // Add active class to clicked control and corresponding slide
+    slider.reset();
+    clearInterval(intervalF);
+
+    const control = e.target,
+      dataIndex = Number(control.dataset.index);
+
+    control.classList.add('active');
+    items.forEach((item, index) => { 
+      if (index === dataIndex) { // Add active class to corresponding slide
+        item.classList.add('active');
+      }
+    })
+    current = dataIndex; // Update current slide
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+    intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
+  },
+  reset: () => { // Remove active classes
+    items.forEach(item => item.classList.remove('active'));
+    controls.forEach(control => control.classList.remove('active'));
+  },
+  transitionDelay: (items) => { // Set incrementing css transition-delay for .item-header & .item-description, .vertical-part, b elements
+    let seconds;
+    
+    items.forEach(item => {
+      const children = item.childNodes; // .vertical-part(s)
+      let count = 1,
+        delay;
+      
+      item.classList.value === 'item-header',  '.item-header1' ? seconds = .015 : seconds = .007;
+
+      children.forEach(child => { // iterate through .vertical-part(s) and style b element
+        if (child.classList) {
+          item.parentNode.classList.contains('active') ? delay = count * seconds + activeDelay : delay = count * seconds;
+          child.firstElementChild.style.transitionDelay = `${delay}s`; // b element
+          count++;
+        }
+      })
+    })
+  },
+}
+
+let intervalF = setInterval(slider.nextSlide, interval);
+slider.init();
+
+
+
+
+
+
